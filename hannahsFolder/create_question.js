c25dc75f79024fffb0213e95977da561b8d603ee
Hannah Dougherty <Hannah.dougherty1@marist.edu>
2019-10-07 12:56:30 -0400

    new updates to create question

--- hannahsFolder/create_question.js
+++ hannahsFolder/create_question.js
@@ -1,3 +1,16 @@
 function createQuestion(){
-    
+    var quest = q.value.length;
+    if(quest == 0){
+        alert("Empty question not valid");
+    }else{
+        var question = document.getElementById("createQ");
+    }
+    var questionT = qt.value.length;
+    if(questionT == 0){
+        alert("Empty question type not valid");
+    }else{
+        var questionType = document.getElementById("createQ");
+    }	
 }
+
+ 
