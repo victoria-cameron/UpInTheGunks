3f66bceb874fa50a6bd05cee76199c0243e6a50d
victoria-cameron <victoria.cameron2@marist.edu>
2019-10-08 10:25:06 -0400

    Whole project wrapped in express and nodejs.

---
+++ server.js
@@ -0,0 +1,32 @@
+﻿const express = require('express');
+const app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var cons = require('consolidate');
+var path = require('path');
+
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+//Connecting to pages
+app.use(express.static(__dirname + '/public/'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.render('login', {
+        title: 'LogIn'
+    });
+});
+
+app.get('/index', (req, res) => {
+    res.render('index.html', {
+        title: 'Index'
+    });
+});
+
+const server = app.listen(5000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
