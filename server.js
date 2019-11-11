93dfbc5515e0ede05ea43751156ad5b93464da0c
Molly Smith <molly.smith4@marist.edu>
2019-11-10 23:34:56 -0500

    Revert "Merge branch 'master' of https://github.com/victoria-cameron/UpInTheGunks"
    This reverts commit 608a7fae51d6c0e13ee2eb6b113ceb88c4fc202e, reversing
    changes made to 00a92f00c00b64a6cea8c8a92a5f5e765dae4006.

--- server.js
+++ server.js
@@ -69,12 +69,6 @@ app.get('/admin_space', (req, res) => {
     });
 });
 
-app.get('/user_space', (req, res) => {
-    res.render('user_space.html', {
-        title: 'User Space'
-    });
-});
-
 app.get('/homepage', (req, res) => {
     res.render('homepage.html', {
         title: 'Home Page'
